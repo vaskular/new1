b2a2ffd6a2898aa9e94afbcdcfa929ce8aaa68c6
Vaskular <zaslanyj@yandex.ru>
2022-06-14 19:27:07 +0300

    HomeWork_2

---
+++ src/HomeWork_2.java
@@ -0,0 +1,41 @@
+public class HomeWork_2 {
+    public static void main(String[] args) {
+        // Задание 1
+        for (int i = 1; i < 101; i++)
+            System.out.println(i + "a");
+
+        // Задание 2
+
+            int ageChildren = 11;
+            if (ageChildren <= 6) {
+                System.out.println("пошел в сад");
+            }
+            if (7 <= ageChildren == ageChildren <= 10) {
+                System.out.println("пошел в начальную школу");
+            }
+            if (11 <= ageChildren == ageChildren <= 16) {
+                System.out.println("пошел в среднюю школу");
+            }
+            if (ageChildren >= 17) {
+                System.out.println("пошел в институт");
+            }
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
